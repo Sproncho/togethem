54a839bfaec64d0fb1766585d1b80a28368087cd
Leonid <ajer333@gmail.com>
2021-03-18 13:44:24 +0200

    LoginPage jsx added

---
+++ src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import './LoginPage.css'
+
+
+export default function LoginPage(){
+    return <div>
+        <input type="text" name="userName" placeholder="type E-mail"/>
+        <input type="password" name="password" placeholder="type password"/>
+        <button>Register</button>
+        <button>Login</button>
+    </div>
+}
