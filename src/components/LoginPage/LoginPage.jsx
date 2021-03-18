a04d2f8da2678de1f19ceadda9b32a4390f4adab
Daniel Zaborov <dan.zaborov@gmail.com>
2021-03-18 14:09:54 +0200

    LoginPage simple Css added

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -2,10 +2,10 @@ import './LoginPage.css'
 
 
 export default function LoginPage(){
-    return <div>
+    return <div className="loginPage">
         <input type="text" name="userName" placeholder="type E-mail"/>
         <input type="password" name="password" placeholder="type password"/>
-        <button>Register</button>
-        <button>Login</button>
+        <button id="registerButton">Register</button>
+        <button id="loginButton">Login</button>
     </div>
 }
