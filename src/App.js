ca916456ad64e178534b2350306a95bc8fda4c4e
Daniel Zaborov <dan.zaborov@gmail.com>
2021-03-18 12:33:27 +0200

    finished Mainpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import MainPage from './components/MainPage';
 import Registration from './components/RegistrationPage';
 
 function App() {
   return (
     <div className="App">
       <Registration/>
+      <MainPage/>
     </div>
   );
 }
