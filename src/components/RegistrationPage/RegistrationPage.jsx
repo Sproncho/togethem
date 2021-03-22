c49acc4fd532ba591aa928551f97ed1939fe991c
Leonid <ajer333@gmail.com>
2021-03-22 14:28:53 +0200

    onSubmit

--- src/components/RegistrationPage/RegistrationPage.jsx
+++ src/components/RegistrationPage/RegistrationPage.jsx
@@ -1,19 +1,74 @@
-import './RegistrationPage.css'
+import "./RegistrationPage.css";
+import { Formik } from "formik";
 
-export default function RegistrationPage(){
-    return <div className="RegistrationPage">
-        <form action="/registration" method="post">
-            <input type="text" name="E-mail" placeholder="type E-mail"/>
-            <input type="text" name="userName" placeholder="type username"/>
-            <input type="password" name="password" placeholder="type password"/>
-            <input type="password" name="password" placeholder="confirm password"/>
-            <select name="userChoise" id="" required>
-                <option value="consumer">Consumer</option>
-                <option value="seller">Seller</option>
-            </select>
-            <br/>
-            <button id="registerButton">Register</button>
-        </form>
-        
+export default function RegistrationPage() {
+  return (
+    <div className="RegistrationPage">
+      <Formik
+        initialValues={{
+          email: "",
+          userName: "",
+          password: "",
+          confirmPassword: "",
+          userChoise: "",
+        }}
+        onSubmit={(values) => {
+          
+        }}
+
+        validate={values => {
+          console.log("validate")
+          const errors = {}
+          return errors
+        }}
+      >
+        {(props) => {
+          console.log(props)
+          return (
+            <form
+              method="post"
+              onSubmit={props.handleSubmit}
+            >
+              <input
+                type="text"
+                name="email"
+                placeholder="type E-mail"
+                value={props.values.email}
+                onChange={props.handleChange}
+              />
+              <input 
+                type="text" 
+                name="userName" 
+                placeholder="type username" 
+                value={props.values.userName}
+                onChange={props.handleChange}
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="type password"
+                value={props.values.password}
+                onChange={props.handleChange}
+              />
+              <input
+                type="password"
+                name="confirmPassword"
+                placeholder="confirm password"
+                value={props.values.confirmPassword}
+                onChange={props.handleChange}
+              />
+              <select name="userChoise" id="" onChange={props.handleChange} required>
+                <option
+                  value="consumer">Consumer</option>
+                <option
+                  value="seller">Seller</option>
+              </select>
+              <br />
+              <button id="registerButton">Register</button>
+            </form>
+          );
+        }}
+      </Formik>
     </div>
+  );
 }
