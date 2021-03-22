5f39431a7c0155cfc4fa8310f089d8fe3b4fa220
Leonid <ajer333@gmail.com>
2021-03-22 12:23:40 +0200

    added form

--- src/components/RegistrationPage/RegistrationPage.jsx
+++ src/components/RegistrationPage/RegistrationPage.jsx
@@ -2,15 +2,18 @@ import './RegistrationPage.css'
 
 export default function RegistrationPage(){
     return <div className="RegistrationPage">
-        <input type="text" name="E-mail" placeholder="type E-mail"/>
-        <input type="text" name="userName" placeholder="type username"/>
-        <input type="password" name="password" placeholder="type password"/>
-        <input type="password" name="password" placeholder="confirm password"/>
-        <select name="userChoise" id="" required>
-            <option value="consumer">Consumer</option>
-            <option value="seller">Seller</option>
-        </select>
-        <br/>
-        <button id="registerButton">Register</button>
+        <form action="/registration" method="post">
+            <input type="text" name="E-mail" placeholder="type E-mail"/>
+            <input type="text" name="userName" placeholder="type username"/>
+            <input type="password" name="password" placeholder="type password"/>
+            <input type="password" name="password" placeholder="confirm password"/>
+            <select name="userChoise" id="" required>
+                <option value="consumer">Consumer</option>
+                <option value="seller">Seller</option>
+            </select>
+            <br/>
+            <button id="registerButton">Register</button>
+        </form>
+        
     </div>
 }
