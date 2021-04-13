be54e25623fd4091b1ca5459504d6c7232b9358d
Daniel Zaborov <dan.zaborov@gmail.com>
2021-04-13 17:09:38 +0300

    added getting lots from firebase

--- src/services/card-data-servcie.js
+++ src/services/card-data-servcie.js
@@ -1,5 +1,6 @@
 import {fb} from '../config/firebase-config';
 import firebase from "firebase";
+import { func } from 'prop-types';
 export async function uploadLot(title,description,soloPrice,amount,photoIDs,hashtags,uid){
     try{
         const collection = fb.firestore().collection("users").doc(uid).collection("lots");
@@ -9,9 +10,30 @@ export async function uploadLot(title,description,soloPrice,amount,photoIDs,hash
             soloPrice,
             amount,
             photoIDs,
-            hashtags
+            hashtags,
+            sellerId:uid
         })
     }catch(error){
         return Promise.reject(error);
+    }
+}
+
+export async function getLots(uid){
+    try{
+        const collection = await fb.firestore().collection("users").doc(uid).collection("lots").get();
+        const lots = collection.docs.map(doc => { return {...doc.data(),id:doc.id}})
+        console.log(lots);
+        return lots; 
+    }catch(error){
+        Promise.reject(error);
+    }
+}
+
+export async function getLotById(uid,id){
+    try{
+        const lot = await fb.firestore().collection("users").doc(uid).collection("lots").doc(id).get();
+        console.log({...lot.data(),id:id});
+    }catch(error){
+        Promise.reject(error);
     }
 }
