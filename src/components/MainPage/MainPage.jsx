379ff0cde50eb72ae3798e20e5713219273c6696
Leonid <ajer333@gmail.com>
2021-04-17 17:18:41 +0300

    added css changes

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,21 +1,32 @@
 import "./MainPage.css";
-import Good from '../Good/Good.jsx';
+import Good from "../Good/Good.jsx";
 import { useHistory } from "react-router-dom";
-import {getLots} from '../../services/card-data-servcie'
-import {useEffect,useState} from 'react';
-export default function MainPage(){
-    const history = useHistory();
-    const [lots,setLots] = useState([]);
-    const [loading,setLoading] = useState(true);
-    useEffect(()=>{
-        setLoading(true);
-        getLots().then(response => {setLots(response)})
-        setLoading(false);
-    },[])
-    return (
-        <div className="MainPage">
-            {loading && <h2>Loading...</h2>}
-            {!loading && lots.map(lot => <Good className="Good" title={lot.title} soloPrice={lot.soloPrice} description = {lot.description} imageId={lot.photoIDs[0]}/>)}
-        </div>
-    )
+import { getLots } from "../../services/card-data-servcie";
+import { useEffect, useState } from "react";
+export default function MainPage() {
+  const history = useHistory();
+  const [lots, setLots] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    getLots().then((response) => {
+      setLots(response);
+    });
+    setLoading(false);
+  }, []);
+  return (
+    <div className="MainPage">
+      {loading && <h2>Loading...</h2>}
+      {!loading &&
+        lots.map((lot) => (
+          <Good
+            className="Good"
+            title={lot.title}
+            soloPrice={lot.soloPrice}
+            description={lot.description}
+            imageId={lot.photoIDs[0]}
+          />
+        ))}
+    </div>
+  );
 }
